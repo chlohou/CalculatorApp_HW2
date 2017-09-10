4589b0f990537a349a52774b8c3a6e2d6ca3be30
Chloe Houlihan <chouliha@asu.edu>
2017-09-10 15:12:02 -0700

    Commit all additional changes

---
+++ CalculatorApp_HW2/Compute.swift
@@ -0,0 +1,33 @@
+//
+//  Compute.swift
+//  CalculatorApp_HW2
+//
+//  Created by Chloe Houlihan on 9/10/17.
+//  Copyright © 2017 Chloe Houlihan. All rights reserved.
+//
+
+import Foundation
+
+class Compute {
+    
+    func add(a: Double, b: Double) -> Double {
+        let answer = a + b
+        return answer
+    }
+    
+    func subtract(a: Double, b: Double) -> Double {
+        let answer = a - b
+        return answer
+    }
+    
+    func multiply(a: Double, b: Double) -> Double {
+        let answer = a * b
+        return answer
+    }
+    
+    func divide(a: Double, b: Double) -> Double {
+        let answer = a / b
+        return answer
+    }
+    
+}
---
+++ CalculatorApp_HW2/Operations.swift
@@ -0,0 +1,20 @@
+//
+//  Operations.swift
+//  CalculatorApp_HW2
+//
+//  Created by Chloe Houlihan on 9/8/17.
+//  Copyright © 2017 Chloe Houlihan. All rights reserved.
+//
+
+import Foundation
+
+
+class Operations {
+
+    
+    func operation (previousValue: Double, nextValue: Double) -> Double {
+        return answerValue
+    }
+    
+    
+}
